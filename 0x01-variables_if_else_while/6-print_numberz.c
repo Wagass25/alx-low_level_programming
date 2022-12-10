03878c477b45205a7e20dd0a3da429db93225a6c
Wagass25 <agalikhenry22@gmail.com>
2022-12-10 01:30:29 -0800

    alx commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + 'a');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
