25baf6f7011ffa9734a65d876958433db9778602
Wagass25 <agalikhenry22@gmail.com>
2022-12-14 15:39:24 -0800

    alx commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower -> checks for lowercase character
+ * @c: character
+ * Return: 1 or 0 depending on condition
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
