1e5a3541724c3d22a4f60ad494922e4ac35f1ac3
Wagass25 <agalikhenry22@gmail.com>
2022-12-14 15:28:40 -0800

    alx commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "mani.h"
+/**
+ * print_alphabet_x10 -> prints the lower case alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; X++)
+	{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
+	}
+	_putchar('\n')
+	}
+}
