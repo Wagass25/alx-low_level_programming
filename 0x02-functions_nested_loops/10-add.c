b520f7a269d2f3c56ffbd15628eac703f0f003df
Wagass25 <agalikhenry22@gmail.com>
2022-12-14 18:09:00 -0800

    alx commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - add two numbers
- * @a: the first number 
- * @b: the second number 
+ * @a: the first number
+ * @b: the second number
  * Return: the addition of both
  */
 int add (int a, int b)
