ac658b1d4bf66fba81c3d428157ef1fedca7f5da
Wagass25 <agalikhenry22@gmail.com>
2022-12-14 16:10:40 -0800

    alx commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a letter
+ * @n: a character to be checked on
+ * Return: 1,0 0r -1 and print their sign
+ */
+int print_sing(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('0');
+return (-1);
+}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: parameter
+ * Return: Absolute value of a number
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+n = -n;
+return (n);
+}
