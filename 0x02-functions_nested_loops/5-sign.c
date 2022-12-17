ddf154cecbbf2de799df06c3444ac2eb86358b5d
Wagass25 <agalikhenry22@gmail.com>
2022-12-17 06:18:51 -0800

    alx commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,12 @@
-#include <limits.h>
 #include "main.h"
 /**
  * print_sign - prints the sign of a letter
+ *
  * @n: a character to be checked on
+ *
  * Return: 1,0 0r -1 and print their sign
  */
+
 int print_sing(int n)
 {
 if (n > 0)
@@ -12,7 +14,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n == 0)
+if (n == 0)
 {
 _putchar('0');
 return (0);
