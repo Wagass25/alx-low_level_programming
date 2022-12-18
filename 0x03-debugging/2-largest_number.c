7d69d965588d25af8a4f9a26b5c24360217f912e
Wagass25 <agalikhenry22@gmail.com>
2022-12-18 04:54:35 -0800

    alx commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,20 +11,24 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-
-	else if (b > c)
+	else if (b > c && b > a)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else 
 	{
 		largest = b;
 	}
 
-	else
-	{
-		largest = c;
-	}
 
 	return (largest)
 }
