0b6edd174c6f9bcd3a305d8fd57e591d4f2e72f9
Wagass25 <agalikhenry22@gmail.com>
2022-12-19 15:01:33 -0800

    alx commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,12 @@
 
 void _puts(char *str)
 {
-	int i = 0
+int i = 0
 
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
