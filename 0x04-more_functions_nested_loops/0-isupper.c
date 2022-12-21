1fef2f0780a0492f231e3e3e69088950822b6971
Wagass25 <agalikhenry22@gmail.com>
2022-12-21 01:06:18 -0800

    alx commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _isupper - checks for uppercase character
  * @c: the character to be checked
@@ -9,5 +9,11 @@
 
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	{
+	return (0);
+	}
 }
