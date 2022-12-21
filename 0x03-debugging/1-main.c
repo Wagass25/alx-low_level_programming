28447eea2b13faaa838302fa8d3012ca5a9cba7a
Wagass25 <agalikhenry22@gmail.com>
2022-12-21 00:52:29 -0800

    alx commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,18 +6,17 @@
  */
 
 int main(void)
-
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
-* while (i < 10) - loop to be run
-* {
-* putchar(i);
-* }
+/*
+*while (i < 10) - loop to be run
+*{
+*putchar(i);
+*}
 */
 printf("Infinite loop avoided! \\o/\n");
 
