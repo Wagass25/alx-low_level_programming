d74733400d2524422788366409840b65494de22a
Wagass25 <agalikhenry22@gmail.com>
2022-12-20 16:04:39 -0800

    alx commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: nothing
+ */
+
+int main(void)
+{
+int counter = 2;
+unsigned long int a = 1;
+unsigned long int b = a + 1;
+unsigned long int c = a + b;
+
+printf("%lu, ", a);
+printf("%lu, ", b);
+while (counter <= 98)
+{
+counter++;
+printf("%lu", c);
+a = b;
+b = c;
+c = a + b;
+if (counter < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
+}
