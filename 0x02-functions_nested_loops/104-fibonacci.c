506f88ed187873847f1ba7bdf4839bd092d49903
Wagass25 <agalikhenry22@gmail.com>
2022-12-20 16:32:38 -0800

    alx commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * Return: nothing
+ *main - finds and prints the first 98 Fibonacci numbers
+ *
+ *Return: 0
  */
 
 int main(void)
 {
-int counter = 2;
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
 
-unsigned long int a = 1;
-unsigned long int b = a + 1;
-unsigned long int c = a + b;
-
-printf("%lu, ", a);
-printf("%lu, ", b);
-while (counter <= 98)
+printf("%ld", bef);
+for (i = 1; i < 91; i++)
 {
-counter++;
-printf("%lu", c);
-a = b;
-b = c;
-c = a + b;
-if (counter < 98)
-{
-printf(", ");
+printf(", %ld", aft);
+aft += bef;
+bef = aft - bef;
 }
+bef1 = (bef / l);
+bef2 = (bef % l);
+aft1 = (aft / l);
+aft2 = (aft % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %ld", aft1 + (aft2 / l));
+printf("%ld", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
 }
 printf("\n");
 return (0);
