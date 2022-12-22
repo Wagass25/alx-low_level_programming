e7d5c05374ab61082ced6a1bb59d5e0ef4a3bf39
Wagass25 <agalikhenry22@gmail.com>
2022-12-22 11:38:29 -0800

    alx commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,7 +10,7 @@ int *p;
 a[2] = 1024;
 p = &n;
 
-/**
+/*
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
@@ -21,7 +21,7 @@ p = &n;
 
 *(p + 5) = 98;
 
-/** ...so that this prints 98\n */
+/* ...so that this prints 98\n */
 
 printf("a[2] = %d\n", a[2]);
 return (0);
